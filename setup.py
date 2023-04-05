bf124d609a8eeec01aa43799b4fac6e73590f4f5
Christian Monch <christian.moench@web.de>
2023-04-05 05:56:34 +0200

    move settings from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python
 
-
 from distutils.core import setup
 
-
-setup(
-    name='video-analyzer',
-    version='0.1',
-    description='Neuron Burst Analyzer',
-    author='Christian Mönch',
-    author_email='christian.moench@web.de',
-    packages=['video_analyzer']
-)
+setup()
